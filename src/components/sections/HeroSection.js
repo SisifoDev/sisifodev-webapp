7ac9ccc571f238291804569ffe597d36efb4aa3d
Sisifo-G <manuelperdomo1710@icloud.com>
2021-03-04 13:48:53 -0500

    Button Component

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -3,6 +3,7 @@ import { Link } from "gatsby";
 import styled from "styled-components";
 import { H1, MediumText } from "../styles/TextStyles";
 import { themes } from "../styles/ColorStyles";
+import PurchaseButton from "../buttons/PurchaseButton";
 
 function HeroSection() {
   return (
@@ -17,6 +18,10 @@ function HeroSection() {
             Don’t skip design. Learn design and code, by building real apps with
             React and Swift. Complete courses about the best tools.
           </Description>
+          <PurchaseButton
+            title="Start Learning Now"
+            subtitle="120+ Hours of video"
+          />
         </TextWrapper>
       </ContentWrapper>
     </Wrapper>
