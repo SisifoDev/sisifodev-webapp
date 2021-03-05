570480937cf56054bf65d982021cdcc91282d950
Sisifo-G <manuelperdomo1710@icloud.com>
2021-03-05 15:55:23 -0500

    404 Page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,32 @@
-import React from "react"
-import Layout from "../components/layout/layout"
-import SEO from "../components/layout/seo"
+import React from "react";
+import Layout from "../components/layout/layout";
+import SEO from "../components/layout/seo";
+import styled from "styled-components";
 
 function NotFoundPage() {
   return (
     <Layout>
       <SEO title="404: Not found" />
-      <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <ImgContainer>
+        <ImgBackground
+          src="/images/404/404.svg"
+          alt="Error 404 Not found Image"
+          align="center"
+        />
+      </ImgContainer>
     </Layout>
-  )
+  );
 }
 
-export default NotFoundPage
+export default NotFoundPage;
+
+const ImgContainer = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ImgBackground = styled.img`
+  width: 400px;
+`;
