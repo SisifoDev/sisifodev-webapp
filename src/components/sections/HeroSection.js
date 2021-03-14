950949dd55a6b5d799ba931ca5081f79d0f1b225
Sisifo-G <manuelperdomo1710@icloud.com>
2021-03-14 17:05:28 -0500

    Cambios en el hero

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -13,17 +13,15 @@ function HeroSection() {
       <ContentWrapper>
         <TextWrapper>
           <Title>
-            Design
-            <br /> and code <span>React</span> apps
+            Bienvenido
+            <br />
+            <span>SisifoDev</span>
           </Title>
           <Description>
-            Don’t skip design. Learn design and code, by building real apps with
-            React and Swift. Complete courses about the best tools.
+            Aquí podrás encontrar cursos, contenido gratuito, tutoriales y otras
+            más que pueden interesarte si eres apasionado por el desarrollo web.
           </Description>
-          <PurchaseButton
-            title="Start learning"
-            subtitle="120+ hours of video"
-          />
+          <PurchaseButton title="Tutoriales" subtitle="120+ horas de videos" />
         </TextWrapper>
         <MockupAnimation />
       </ContentWrapper>
--- src/data/menuData.js
+++ src/data/menuData.js
@@ -1,17 +1,21 @@
 export const menuData = [
-  { title: "Courses", icon: "/images/icons/courses.svg", link: "/courses" },
+  { title: "Cursos", icon: "/images/icons/courses.svg", link: "/courses" },
   {
-    title: "Tutorials",
+    title: "Tutoriales",
     icon: "/images/icons/tutorials.svg",
     link: "/tutorials",
   },
-  { title: "Pricing", icon: "/images/icons/pricing.svg", link: "/pricing" },
+  { title: "Blog", icon: "/images/icons/pricing.svg", link: "/pricing" },
   { title: "", icon: "/images/icons/search.svg", link: "/search" },
   { title: "", icon: "/images/icons/account.svg", link: "/account" },
-]
+];
 
 export const tooltipData = [
-  { title: "Profile", icon: "/images/icons/profile.svg", link: "/profile" },
-  { title: "Settings", icon: "/images/icons/settings.svg", link: "/settings" },
-  { title: "Sign out", icon: "/images/icons/signout.svg", link: "/logout" },
-]
+  { title: "Usuario", icon: "/images/icons/profile.svg", link: "/profile" },
+  {
+    title: "Configuración",
+    icon: "/images/icons/settings.svg",
+    link: "/settings",
+  },
+  { title: "Salir", icon: "/images/icons/signout.svg", link: "/logout" },
+];
